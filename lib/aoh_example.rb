c400ccc9f4eb543cfe105b3d57e9faa8252f72fd
Lee Bardon <leerbardon@gmail.com>
2019-12-22 22:27:17 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,9 +25,9 @@ def literal_aoh
   
   literal_array = [
     { name:  "Don Gately", occupation: "Live-in Staff" },
-    JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+    { name:  "Pat Monteseian", occupation: "Staff" }
+  KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end

d28701c64c79a2f6ec60342972afc492559efb39
Lee Bardon <leerbardon@gmail.com>
2019-12-22 22:16:34 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,7 @@ def assembled_aoh
 [ 
   DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
   JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
+  PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end

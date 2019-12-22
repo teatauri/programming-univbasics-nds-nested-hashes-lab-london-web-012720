cb07637c8d2f3d100f8b8332c706a8de7018d8c4
Lee Bardon <leerbardon@gmail.com>
2019-12-22 22:16:52 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,8 +14,8 @@ def assembled_aoh
   DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
   JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
   PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
+  KATE_G = { name:  "Kate Gompert", occupation: "None" }
+  BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
 
 def literal_aoh
